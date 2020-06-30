97bf4957b635b682476ff019212ab97e8bc53644
Kyle Belveal <k.belveal17@gmail.com>
2020-06-30 16:34:12 -0400

    verbosity

--- .sh/build.sh
+++ .sh/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# script revised: Tue June 30, 2020 @ 04:28:43 EDT
+# script revised: Tue June 30, 2020 @ 04:34:02 EDT
 
 sudo cp $(git rev-parse --show-toplevel)/kbv.conf /etc/httpd/sites-available/kbv.conf
 sudo cp -r $(git rev-parse --show-toplevel)/keelbeelveel.github.io/* /var/www/kbv/
+echo ("site built from keelbeelveel.github.io.")
 sudo systemctl reload httpd
+echo ("httpd reloaded.")
