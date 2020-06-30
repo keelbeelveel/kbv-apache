859bf7bfeb7207c19cbcc632c36c5aacf340234c
Kyle Belveal <k.belveal17@gmail.com>
2020-06-30 16:03:14 -0400

    added autocopy and autoreload

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# script revised: Tue June 30, 2020 @ 03:51:06 EDT
+# script revised: Tue June 30, 2020 @ 04:03:00 EDT
 
 sudo cp kbv.conf /etc/httpd/sites-available/kbv.conf
-
+sudo cp -r keelbeelveel.github.io/* /var/www/kbv/
+sudo systemctl reload httpd
