000e64072860db1bb1d05d3c6f5a1b8e67ac987b
Kyle Belveal <k.belveal17@gmail.com>
2020-06-30 16:47:55 -0400

    typo

--- .sh/flag-available.sh
+++ .sh/flag-available.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# script modified: Tue June 30, 2020 @ 03:55:03 EDT
+# script modified: Tue June 30, 2020 @ 04:47:48 EDT
 
 sudo ln -s /etc/httpd/sites-available/kbv.conf /etc/httpd/sites-enabled/kbv.conf
 echo "kbv.conf has been symlinked into /etc/httpd/sites-enabled"
-echo "Run /.flag-unavailable to remove this link."
+echo "Run ./flag-unavailable to remove this link."
 
