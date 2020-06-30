0812a4caa54798331a23b84eb89579484187de2c
Kyle Belveal <k.belveal17@gmail.com>
2020-06-30 17:57:16 -0400

    $(git toplevel) absolute paths

--- .sh/rebuild.sh
+++ .sh/rebuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Script modified: Tue June 30, 2020 @ 04:46:23 EDT
-./clean.sh 
-./build.sh
+# Script modified: Tue June 30, 2020 @ 05:56:28 EDT
+$(git rev-parse --show-toplevel)/clean.sh 
+$(git rev-parse --show-toplevel)/build.sh
 sudo systemctl reload httpd
