e7ead78078061ff6274a5c4c3c98c050e2753dbe
Kyle Belveal <k.belveal17@gmail.com>
2020-06-30 16:45:43 -0400

    fancier output

--- .sh/build.sh
+++ .sh/build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-# script revised: Tue June 30, 2020 @ 04:43:18 EDT
+# script revised: Tue June 30, 2020 @ 04:45:34 EDT
 echo "  "
 echo "######################"
 echo " build.sh"
 echo "######################"
 echo "  "
-sudo cp $(git rev-parse --show-toplevel)/kbv.conf /etc/httpd/sites-available/kbv.conf
-sudo cp -r $(git rev-parse --show-toplevel)/keelbeelveel.github.io/* /var/www/kbv/
+sudo cp -v $(git rev-parse --show-toplevel)/kbv.conf /etc/httpd/sites-available/kbv.conf
+sudo cp -rv $(git rev-parse --show-toplevel)/keelbeelveel.github.io/* /var/www/kbv/
+echo "  "
+echo "--------------------"
+echo "  "
 echo "site built from keelbeelveel.github.io."
 sudo systemctl reload httpd
 echo "httpd reloaded."
--- .sh/clean.sh
+++ .sh/clean.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-# Script modified: Tue June 30, 2020 @ 04:42:49 EDT
+# Script modified: Tue June 30, 2020 @ 04:45:34 EDT
 echo " " 
 echo "######################"
 echo " clean.sh"
 echo "######################"
 echo " " 
 sudo rm -rv /var/www/kbv/*
+echo "  "
+echo "--------------------"
+echo "  "
 echo "/var/www/kbv Cleaned. Run ./build.sh to Rebuild."
 echo "  "
 echo "===================="
